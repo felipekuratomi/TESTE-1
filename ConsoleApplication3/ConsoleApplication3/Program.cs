56a9884cba356f63e4eed5227b6ad44f66367e10
felipekuratomi <f.ikari.kuratomi@avanade.com>
2015-02-20 18:07:27 -0200

    20 02 15

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    enum Dia
+    { 
+        Domingo,
+        Segunda,
+        Terca
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processar(Dia.Segunda);
+        }
+        static void Processar(Dia dia)
+        {
+            switch (dia)
+            {
+                case Dia.Domingo:
+                    break;
+                case Dia.Segunda:
+                    break;
+                case Dia.Terca:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
