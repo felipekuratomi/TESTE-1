34a85e01ccd029fd9cf3c6c1b5a48267954ab050
felipekuratomi <f.ikari.kuratomi@avanade.com>
2015-02-23 13:12:20 -0300

    Almoço 23/02

---
+++ Heranca  Demo/Heranca  Demo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca__Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var c = new Cachorro();
+            //c.Nome = "Fido";
+            //c.Andar();
+            CuidadorDeAnimais.LevarParaPassear(c);
+
+            var g = new Gato();
+            CuidadorDeAnimais.LevarParaPassear(g);
+            Console.Read();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract void Falar();
+
+        public string Nome { get; set; }
+        public virtual void Andar()
+        {
+            Console.WriteLine("Animal andando");
+        
+        }
+
+    }
+
+    class CuidadorDeAnimais
+    {
+        public static void LevarParaPassear(Animal animal)
+        {
+            animal.Andar();
+        }
+        
+    }
+
+
+
+
+    sealed class Cachorro : Animal
+    {
+        public override void Andar()
+        {
+            base.Andar();
+            Console.WriteLine("Cachorro andando");
+        }
+        public override void Falar()
+        {
+            throw new NotImplementedException();
+        }
+        
+    
+    }
+    sealed class Gato : Animal
+    {
+        public override void Andar()
+        {
+            Console.WriteLine("Gato andando");
+        }
+        public override void Falar()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+
+
+}
---
+++ TestesDemo/TestsDemo.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestesDemo;
+
+namespace TestsDemo.Tests
+{
+    [TestClass] //Atributos
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Adiciona_2_e_2_retorna_4()
+        {
+            // Arrange
+            var calc = new Calculadora();
+
+
+            // Act
+            int expect = 4;
+            int actual = calc.Adicionar(2, 2);
+
+
+            // Assert
+            Assert.AreEqual(expect, actual);
+
+
+        }
+    }
+}
